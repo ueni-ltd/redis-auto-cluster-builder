78d741d6cec6e39f85d2e5a7d7517894ddd09cc9
DevLounge <anthonyperot@gmail.com>
2018-02-14 10:18:48 +0000

    added cluster check after cluster creation

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 NODES=$(getent hosts ${REDIS_SERVICE} | awk '{ printf("%s:6379 ", $1) }')
 
 yes "yes" | ./redis-trib.rb create --replicas ${REPLICATION_FACTOR} ${NODES}
+
+./redis-trib.rb check ${REDIS_SERVICE}:6379
