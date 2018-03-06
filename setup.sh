6fdbe6a79fb5e0cdc48cc90157ccccf2826ad785
Anthony Perot <anthonyperot@gmail.com>
2018-03-06 01:39:10 +0000

    wait until the expected number of nodes are ready

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+while [ $(getent hosts ${REDIS_SERVICE} | wc -l) -lt ${CLUSTER_SIZE} ]
+do
+  echo "waiting for the $CLUSTER_SIZE nodes to be ready"
+  sleep 5
+done
+
 NODES=$(getent hosts ${REDIS_SERVICE} | awk '{ printf("%s:6379 ", $1) }')
 
 yes "yes" | ./redis-trib.rb create --replicas ${REPLICATION_FACTOR} ${NODES}
